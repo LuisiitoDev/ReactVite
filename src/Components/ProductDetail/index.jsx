2ba297d55dfa6b31ec5994fc0be8a80138409c50
Luis Sandoval Monestel <79773016+LuisiitoDev@users.noreply.github.com>
2023-06-24 08:58:34 -0600

    it was added product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -4,19 +4,36 @@ import { XMarkIcon } from "@heroicons/react/24/solid";
 import "./styles.css";
 
 const ProductDetail = () => {
-  const { isProductDetailOpen, closeProductDetail } =
+  const { isProductDetailOpen, closeProductDetail, productToShow } =
     useContext(ShoppingCartContext);
 
   return (
-    <aside className={`${isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-10 border border-black rounded-lg bg-white`}>
+    <aside
+      className={`${
+        isProductDetailOpen ? "flex" : "hidden"
+      } product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
           <XMarkIcon
-            onClick={() => closeProductDetail()} 
-            className="h-6 w-6 text-black"></XMarkIcon>
+            onClick={() => closeProductDetail()}
+            className="h-6 w-6 text-black"
+          ></XMarkIcon>
         </div>
       </div>
+      <figure className="px-6">
+        <img
+          className="w-full h-full rounded-lg"
+          src={productToShow.images[0]}
+          alt={productToShow.title}
+        />
+      </figure>
+      <p className="flex flex-col p-6">
+        <span className="font-medium text-2xl">${productToShow.price}</span>
+        <span className="font-medium text-md">{productToShow.title}</span>
+        <span className="font-light text-sm">{productToShow.description}</span>
+      </p>
     </aside>
   );
 };
